eac3cd0e81b15a70f7214cea678caf0577194b91
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-05-03 15:06:16 +0100

    Passed tests for tag and content request queries

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
 const {Joke} = require('./Joke');
-const {sequelize, Sequelize} = require('./db');
+const {sequelize, Sequelize, Op} = require('./db');
 
 module.exports = {
     Joke,
     sequelize,
-    Sequelize
+    Sequelize,
+    Op
 };
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
-
+const { Op } = require("sequelize");
+//  
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
+  if (req.query) {
+    try {
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]: req.query.tags ?? "",
+          },
+          joke: {
+            [Op.substring]: req.query.content ?? "",
+          }
+        },
+      });
+      res.send(jokes); 
+    } catch (error) {
+        console.error(error);
+        next(error)
+    }
+  }
+
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
+      const jokes = await Joke.findAll();
+      res.send(jokes);
   } catch (error) {
-    console.error(error);
-    next(error)
+      console.error(error);
+      next(error)
   }
 });
 
